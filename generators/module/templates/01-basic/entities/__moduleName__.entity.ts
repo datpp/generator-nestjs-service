f727fa6c0493a79329508abaa19c99d0b6268e87
Dat Pham <dat.pham@pops.vn>
2022-05-18 02:49:04 +0700

    - Support OpenAPI
    Signed-off-by: Dat Pham <dat.pham@pops.vn>

--- generators/module/templates/01-basic/entities/__moduleName__.entity.ts
+++ generators/module/templates/01-basic/entities/__moduleName__.entity.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class <%= kebabToPascal(config.name) %>Entity {
   @PrimaryGeneratedColumn()
+  @ApiProperty({ description: 'The id of the <%= kebabToPascal(config.name) %>' })
   id: number;
 
   @Column({ length: 500 })
+  @ApiProperty({ description: 'The name of the <%= kebabToPascal(config.name) %>' })
   name: string;
 }
